0ac8d1aa9235db6a0495cebe10fedf12e8d0909a
mirkhala <mirkhala@gmail.com>
2021-11-19 00:27:39 +0000

    default

--- prediction.py
+++ prediction.py
@@ -312,7 +312,7 @@ def  lpr_process(input_image_path):
         (x, y, w, h) = cv2.boundingRect(c)
         ratio = h/w
         if 1<=ratio<=3.5: # Only select contour with defined ratio
-            if h/plate_image.shape[0]>=0.3: # Select contour which has the height larger than 50% of the plate
+            if h/plate_image.shape[0]>=0.5: # Select contour which has the height larger than 50% of the plate
                 # Draw bounding box arroung digit number
                 cv2.rectangle(test_roi, (x, y), (x + w, y + h), (0, 255,0), 2)
                 # Sperate number and gibe prediction
